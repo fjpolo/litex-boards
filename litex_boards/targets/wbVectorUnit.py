10e6611caf75310bb52b6138cce69d7fc92dbc89
fjpolo <fjpolo@gmail.com>
2025-06-03 16:22:27 +0200

    Add a counter register (will be added into a FSM later)

--- litex_boards/targets/wbVectorUnit.py
+++ litex_boards/targets/wbVectorUnit.py
@@ -3,24 +3,26 @@ from migen import *
 from litex.gen import *
 from litex.soc.interconnect import wishbone
 
-class wbVectorUnit (LiteXModule):
+class wbVectorUnit(LiteXModule):
     def __init__(self, data_width=32):
         self.data_width = data_width
 
         # Wishbone MASTER Interface (for Vector Unit to initiate memory access)
+        # Note: This was previously removed, but your provided file included it again.
+        # Ensure this is commented out or removed in sipeed_tang_nano_20k.py if not in use.
         self.master_bus = wishbone.Interface(data_width=data_width, address_width=32)
 
         # Wishbone SLAVE Interface (for CPU to access Vector Unit's registers)
-        # address_width=3 for 8 registers (0x0-0x7 word addresses) - Still valid for 4 registers (0x00, 0x04, 0x08, 0x0C)
-        self.slave_bus = wishbone.Interface(data_width=data_width, address_width=3)
+        self.slave_bus = wishbone.Interface(data_width=data_width, address_width=8)
 
         # ----------------------------------------------------------------------
         # Internal Registers of the Vector Unit (exposed via Wishbone Slave)
         # ----------------------------------------------------------------------
-        self.status_reg         = Signal(data_width, reset=0)
-        self.control_register   = Signal(data_width, reset=0)
-        self.address_register   = Signal(data_width, reset=0)
-        self.element_count_register = Signal(data_width, reset=0) # <--- NEW REGISTER: holds number of elements (e.g., 8, 12, 32)
+        self.status_reg             = Signal(data_width, reset=0)
+        self.control_register       = Signal(data_width, reset=0)
+        self.address_register       = Signal(data_width, reset=0)
+        self.element_count_register = Signal(data_width, reset=0)
+        self.counter_register       = Signal(data_width, reset=0xFFFFF000)
 
         # Registered signals for Wishbone slave outputs (driven by FSM in sync domain)
         o_ack   = Signal()
@@ -38,6 +40,10 @@ class wbVectorUnit (LiteXModule):
         fsm = FSM(reset_state="IDLE")
         self.submodules += fsm
 
+        # !!! CORE CHANGE: Continuous increment for counter_register !!!
+        # This occurs on every rising edge of the system clock.
+        self.sync += self.counter_register.eq(self.counter_register + 1)
+
         # Define internal FSM states
         fsm.act("IDLE",
             o_ack.eq(0), # Keep ack low by default
@@ -51,24 +57,25 @@ class wbVectorUnit (LiteXModule):
                     If(~self.slave_bus.we, # Read transaction
                         # Actions for Read (grouped in a list if multiple)
                         [
-                            # Reverted to DICTIONARY SYNTAX for Case:
                             Case(self.slave_bus.adr, {
-                                0b000: NextValue(o_dat_r, self.status_reg),     # Offset 0x00
-                                0b001: NextValue(o_dat_r, self.control_register), # Offset 0x04
-                                0b010: NextValue(o_dat_r, self.address_register), # Offset 0x08
-                                0b011: NextValue(o_dat_r, self.element_count_register), # Offset 0x0C <-- NEW READ CASE
-                                "default": NextValue(o_dat_r, 0) # Return 0 for unmapped reads
+                                0b000: NextValue(o_dat_r, self.status_reg),
+                                0b001: NextValue(o_dat_r, self.control_register),
+                                0b010: NextValue(o_dat_r, self.address_register),
+                                0b011: NextValue(o_dat_r, self.element_count_register),
+                                0b100: NextValue(o_dat_r, self.counter_register),
+                                "default": NextValue(o_dat_r, 0xDEADFE05) # Return 0 for unmapped reads
                             })
                         ]
                     ).Else( # Write transaction
                         # Actions for Write (grouped in a list if multiple)
                         [
-                            # Reverted to DICTIONARY SYNTAX for Case:
                             Case(self.slave_bus.adr, {
-                                0b000: NextValue(self.status_reg,     self.slave_bus.dat_w), # Offset 0x00
-                                0b001: NextValue(self.control_register, self.slave_bus.dat_w), # Offset 0x04
-                                0b010: NextValue(self.address_register, self.slave_bus.dat_w), # Offset 0x08
-                                0b011: NextValue(self.element_count_register, self.slave_bus.dat_w), # Offset 0x0C <-- NEW WRITE CASE
+                                0b000: NextValue(self.status_reg,             self.slave_bus.dat_w),
+                                0b001: NextValue(self.control_register,       self.slave_bus.dat_w),
+                                0b010: NextValue(self.address_register,       self.slave_bus.dat_w),
+                                0b011: NextValue(self.element_count_register, self.slave_bus.dat_w),
+                                # !!! REMOVED WRITE ACCESS FOR counter_register !!!
+                                # 0b100: NextValue(self.counter_register,       self.slave_bus.dat_w),
                             }),
                             NextValue(o_dat_r, 0) # For writes, clear data output (or provide read data if needed)
                         ]
